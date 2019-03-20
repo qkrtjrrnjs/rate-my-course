dbef31cf0da506fb43cd9462976e2ca29d143b4b
Seok Kwun Park <park960@purdue.edu>
2019-03-20 19:02:06 -0400

    initial commit

---
+++ rate my course/rate my course/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rate my course
+//
+//  Created by chris on 3/20/19.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
