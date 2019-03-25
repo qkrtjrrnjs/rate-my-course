eeb7b6e705baab65b017f7d44ffd8b52df65eca0
Seok Kwun Park <park960@purdue.edu>
2019-03-25 18:21:48 -0400

    add close keyboard extension

---
+++ rate my course/rate my course/Extensions/shortExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  shortExtensions.swift
+//  rate my course
+//
+//  Created by chris on 3/25/19.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
