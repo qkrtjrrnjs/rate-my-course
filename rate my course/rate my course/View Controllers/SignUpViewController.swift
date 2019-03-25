8c055507aeae95e7c0b6381c01419bff34cb98d7
Seok Kwun Park <park960@purdue.edu>
2019-03-25 14:30:17 -0400

    add textfields, labels, buttons, and references

--- rate my course/rate my course/View Controllers/SignUpViewController.swift
+++ rate my course/rate my course/View Controllers/SignUpViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var verifyPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUp(_ sender: Any) {
     }
-    */
+    
 
 }
